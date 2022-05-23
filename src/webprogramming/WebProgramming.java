bd1552580ded0d5471b9e632c25d3808268be1dd
prabha joyikutty <joyikutt@sheridancollege.ca>
2022-05-23 11:03:32 -0400

    version 1

---
+++ src/webprogramming/WebProgramming.java
@@ -0,0 +1,13 @@
+//https://github.com/joyikutt/week3_winter20200.git
+
+package webprogramming;
+
+
+public class WebProgramming {
+
+    
+    public static void main(String[] args) {
+       System.out.println("Learning GitHub");
+    }
+    
+}
